bc893aba8da0e7ff7eb24d66fac043a088d9b9d7
Nikolazzzer <nikola-tarasov@mail.ru>
2024-09-29 10:54:52 +0300

    добавление логики добавления из корзины в бд в таблицу orders

--- views/pages/orders.php
+++ views/pages/orders.php
@@ -1,9 +1,10 @@
-<h2 align="center">оформления заказов</h2>
+<h2 align="center">Оформления заказов</h2>
 <?php
 
-if($_SESSION['cart']){
+//если есть сессия масси и не нажата кнопка
+if($_SESSION['cart'] && !isset($_POST['order'])){
     echo '
-<form action="index.php?view=order" method="post" id="cart-form">
+<form action="index.php?view=orders" method="post" id="cart-form">
 
     <table id="mycart" align="center" cellspacing="0" cellpadding="0" border="0">
         <tr>
@@ -44,11 +45,59 @@ if($_SESSION['cart']){
             <p align="center" style="color: aqua">Ваше имя <input type="text" name="name" id=""></p>
             <p align="center" style="color: aqua">Ваша фамилиия <input type="text" name="s-name" id=""> </p>
             <p align="center" style="color: aqua">Ваша почтовый индекс <input type="text" name="post-index" id=""> </p>
-            <p align="center" style="color: aqua">Ваш e-mail <input type="email" name="email" id=""> </p>
+            <p align="center" style="color: aqua">Ваш e-mail <input type="text" name="email" id=""> </p>
             <p align="center" style="color: aqua">Ваш адрес <input type="text" name="adress" id=""> </p>
             <p align="center"><input type="submit" name="order" value="Заказать" /></p>
 </form>';
 }
+
+//если есть массив сессии и нажата кнопка order то добавляються из формы в бд таблицу orders
+if ($_SESSION['cart'] && isset($_POST['order'])){
+
+    $name = $_POST['name'];
+    $s_name = $_POST['s-name'];
+    $post_index = $_POST['post-index'];
+    $email = $_POST['email'];
+    $adress = $_POST['adress'];
+    $data = date('Y-m-d');
+    $time = date('H:i:s');
+
+    foreach ($_SESSION['cart'] as $id=>$quantity) {
+
+        $sql = "SELECT * FROM products WHERE id= :id";
+
+        $res = $conn->prepare($sql);
+
+        $res->execute([
+            'id' => $id
+        ]);
+
+        $product = $res->fetch();
+
+        $insert_sql = "INSERT INTO orders (product, prod_id, price, qty, name, s_name, post_index, email, date, time, adress) VALUES (:product, :prod_id, :price, :qty, :name, :s_name, :post_index, :email, :date, :time, :adress)";
+
+        $stmt= $conn->prepare($insert_sql);
+
+        $stmt->execute(['product' => $product['title'],
+            'prod_id' => $product['id'],
+            'price' => $product['price'],
+            'qty' => $quantity,
+            'name' => $name,
+            's_name' => $s_name,
+            'post_index' => $post_index,
+            'email' => $email,
+            'date' => $data,
+            'time' => $time,
+            'adress' => $adress
+        ]);
+    }
+
+//    выводиться если прошла оплата
+    echo "Спасибо! Заказ оформлен!";
+
+}
+
+
 ?>
 
 
